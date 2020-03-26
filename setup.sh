4f6d393b3a4ddb4a40f3a478cb18c7a76625e4e1
deeptir <deeptir@cs.stanford.edu>
2020-03-26 11:03:14 -0700

    oops

--- setup.sh
+++ setup.sh
@@ -22,7 +22,7 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 
 # rust
 curl https://sh.rustup.rs -sSf | sh
-$HOME/.cargo/env
+source $HOME/.cargo/env
 
 # source
 source ~/.bashrc
