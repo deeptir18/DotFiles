a1a64cd188b03d5e659d2a7179d44cb5d4b7b606
deeptir <deeptir@cs.stanford.edu>
2020-03-26 10:59:35 -0700

    Modified for easy setup

--- setup.sh
+++ setup.sh
@@ -3,34 +3,25 @@
 # install vim and zshrc settings
 sudo apt-get install zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-git clone git@github.com:deeptir18/DotFiles.git
 cp DotFiles/vimrc ~/.vimrc
 cp DotFiles/ubuntu_bashrc ~/.bashrc
 cp DotFiles/ubuntu_zshrc ~/.zshrc
+cp DotFiles/tmux_conf ~/.tmux.conf
 
+# vim plug and install vim dependencies
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+vim +PlugInstall +qall
 
+#tpm
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
-#vim +PluginInstall +qall
-sudo apt-get install cmake
+# powerline command line
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
-# setup for you complete me
-cd ~/.vim/bundle/YouCompleteMe
-./install.py --clang-completer
-./install.py --rust-completer
-./install.py --go-completer
-
-cd ..
-
-
-# mahimahi and rust
+# rust
 curl https://sh.rustup.rs -sSf | sh
-
-sudo apt-get install autotools-dev autoconf libtool apache2 apache2-dev protobuf-compiler libprotobuf-dev libssl-dev xcb libxcb-composite0-dev libxcb-present-dev libcairo2-dev libpango1.0-dev
-
-git clone https://github.com/fcangialosi/mahimahi.git
-cd mahimahi  && ./autogen.sh && ./configure && make && sudo make install
 
 
 
