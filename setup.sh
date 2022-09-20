7dc528ce5bc4fa3c4a4fe21219b21aa989ef4d74
Deepti Raghavan <deeptir@cs.stanford.edu>
2022-09-20 10:29:32 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -34,12 +34,11 @@ git config --global user.name "deeptir@cs.stanford.edu"
 
 # install tmux plugins
 # start a server but don't attach to it
-# source ~/.tmux.conf
-# tmux start-server
+tmux start-server
 # create a new session but don't attach to it either
-#tmux new-session -d
+tmux new-session -d
 # install the plugins
 ~/.tmux/plugins/tpm/scripts/install_plugins.sh
 # killing the server is not required, I guess
-#tmux kill-server
+tmux kill-server
 
